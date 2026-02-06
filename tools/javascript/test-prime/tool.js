7e9d094c80eaf9cef90ae17e008869b685edc120
Spirit <alfonsoballon96@icloud.com>
2026-02-06 17:42:10 +0800

    Add source for test-prime

---
+++ tools/javascript/test-prime/tool.js
@@ -0,0 +1,11 @@
+/**
+ * Prime Factorization
+ * 
+ * Decompose a number into its prime factors.
+ * 
+ * @category math
+ * @input { "number": 84 }
+ */
+
+const input = JSON.parse(process.argv[2] || '{}');
+console.log(input);
